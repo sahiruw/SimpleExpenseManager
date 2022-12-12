eb93977ba58aec90a6d9d6260f639505aae05b64
sahiru <sahiruwrcg@gmail.com>
2022-12-13 02:44:48 +0530

    update databse

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentDemoExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentDemoExpenseManager.java
@@ -22,6 +22,7 @@ import java.text.ParseException;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
@@ -46,11 +47,11 @@ public class PersistentDemoExpenseManager extends ExpenseManager {
         AccountDAO persistentAccountDAO = new PersistentAccountDAO(context);
         setAccountsDAO(persistentAccountDAO);
 
-////        // dummy data
-        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
-        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
-        getAccountsDAO().addAccount(dummyAcct1);
-        getAccountsDAO().addAccount(dummyAcct2);
+        // dummy data
+//        Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
+//        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+//        getAccountsDAO().addAccount(dummyAcct1);
+//        getAccountsDAO().addAccount(dummyAcct2);
 
         /*** End ***/
     }
